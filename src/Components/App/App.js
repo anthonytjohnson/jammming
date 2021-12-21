28f5f3b4c55218220f69ca3c9c368bd763ad2e14
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-12-21 13:43:59 +0900

    passed down props from app to playlist tracks and rendered a list of tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
-// import Playlist from '../Playlist/Playlist';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }]
+      searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }],
+      playlistName: "playlist1",
+      playlistTracks: [{ name: "name4", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }]
     }
   }
 
@@ -21,7 +23,7 @@ class App extends React.Component {
            <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults = {this.state.searchResults} />
-            {/* <Playlist /> */}
+            <Playlist playlistTracks = {this.state.playlistTracks} playlistName={this.state.playlistName} />
           </div>
         </div>
       </div>
