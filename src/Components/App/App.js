a1b85a8944ef8d3ea49597eb60b1c481c7e90222
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-12-22 10:40:48 +0900

    user can cow save a playlist to their account

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, { name: "name2", artist: "artist2", album: "album2", id: 2 }, { name: "name3", artist: "artist3", album: "album3", id: 3 }],
-      playlistName: "playlist1",
-      playlistTracks: [{ name: "name4", artist: "artist4", album: "album4", id: 4 }, { name: "name5", artist: "artist5", album: "album5", id: 5 }, { name: "name6", artist: "artist6", album: "album6", id: 6 }]
+      searchResults: [],
+      playlistName: "My Playlist",
+      playlistTracks: []
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -44,7 +44,12 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
-    // return trackURIs;
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
